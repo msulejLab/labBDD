b845818a3491f855eae05a8be4ecae07b20e22b3
Maciej Sulej <187809@edu.p.lodz.pl>
2016-06-09 11:45:54 +0200

    Nadanie struktury klasie StepsCollector

--- src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/StepsCollector.java
+++ src/test/java/edu/iis/mto/bdd/trains/cucumber/steps/StepsCollector.java
@@ -5,27 +5,46 @@ import cucumber.api.java.pl.Gdy;
 import cucumber.api.java.pl.I;
 import cucumber.api.java.pl.Wtedy;
 import cucumber.api.java.pl.Zakładając;
+import edu.iis.mto.bdd.trains.services.InMemoryTimetableService;
+import edu.iis.mto.bdd.trains.services.TimetableService;
+import org.joda.time.LocalTime;
 
-public class EstimatingTime {
+public class StepsCollector {
+    private TimetableService timetableService = new InMemoryTimetableService();
 
+    private String startingPoint;
+    private String destinationPoint;
+    private String line;
+
+    private LocalTime arrivingTime;
 
     @Zakładając("^, że chcę się dostać z \"(.*)\" do \"(.*)\"$")
-    public void givenStartingPointToDestinationPoint(String fromLocalization, String toLocalization) throws Throwable {
+    public void givenStartingPointToDestinationPoint(String startingPoint, String destinationPoint) throws Throwable {
+        this.startingPoint = startingPoint;
+        this.destinationPoint = destinationPoint;
+
         throw new PendingException();
     }
 
     @I("^następny pociąg odjeżdża o (\\d+):(\\d+) na linii \"(.*)\"")
     public void setUpNextTrain(int hours, int minutes, String line) throws Throwable {
+        timetableService.scheduleArrivalTime(line, new LocalTime(hours, minutes));
+        this.line = line;
+
         throw new PendingException();
     }
 
     @Gdy("^zapytam o godzinę przyjazdu$")
     public void whenAskingForArrivingTime() throws Throwable {
+        arrivingTime = timetableService.getArrivalTime(line, destinationPoint);
+
         throw new PendingException();
     }
 
     @Wtedy("^powinienem uzyskać następujący szacowany czas przyjazdu: (\\d+):(\\d+)$")
     public void thenShouldGetArrivingTime(int hours, int minutes) throws Throwable {
+        arrivingTime.equals(new LocalTime(hours, minutes));
+
         throw new PendingException();
     }
 }
